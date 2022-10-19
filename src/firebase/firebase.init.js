dd676b9ff36fe1acaac958d0f73fc1c3acd69e4b
MD RAKIBUL ISLAM RAKIB <rakibulinux@gmail.com>
2022-10-19 14:15:07 +0600

    added link done

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAhv83d6npz9JESNuBNcUF8xbMIyU84Bzs",
+  authDomain: "authentech-98fa0.firebaseapp.com",
+  projectId: "authentech-98fa0",
+  storageBucket: "authentech-98fa0.appspot.com",
+  messagingSenderId: "1024671593425",
+  appId: "1:1024671593425:web:59978a7ff79f6e7514a6f0",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import 'react-toastify/dist/ReactToastify.css'
-import { ToastContainer } from 'react-toastify'
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import UserContext from "./contexts/UserContext";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <>
-    <ToastContainer position='top-center' />
-    <App />
-  </>
-)
+  <React.StrictMode>
+    <UserContext>
+      <ToastContainer position="top-center" />
+      <App />
+    </UserContext>
+  </React.StrictMode>
+);
